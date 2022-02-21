88dcd5969c4f82b384c66abb48d25baecdef6030
Etienne <>
2022-02-20 22:43:18 -0500

    og:description

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const SITE_NAME = "Etienne Martin";
 export const SITE_TITLE = `${SITE_NAME} – Thinking out loud`;
 export const SITE_DESCRIPTION =
-  "An unordered list of random thoughts about web development";
+  "A chronological list of thoughts about web development";
 export const ORIGIN = process.env.ORIGIN || "http://localhost:3000";
 // TODO: create a social image
 export const DEFAULT_SOCIAL_IMAGE = `${ORIGIN}/social-image.png`;
